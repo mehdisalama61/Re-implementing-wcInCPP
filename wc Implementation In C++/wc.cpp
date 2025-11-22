271f87982070b6b469c9f01016b6a146e218b1d8
elmehdisalama61 <mehdisalama61@gmail.com>
2025-11-22 20:07:22 +0100

    step 1 is finished

--- wc Implementation In C++/wc.cpp
+++ wc Implementation In C++/wc.cpp
@@ -8,7 +8,21 @@ int main(){
     file.open("text.txt", ios::in | ios::out | ios :: binary);
     if (file.is_open())
     {
-        cout<<"hello"<<endl;
+        /*
+        
+        string buffer;
+        do
+        {
+            
+            file >> buffer;
+            cout << buffer<< endl;
+            cout<<file.tellg()<<endl;
+        } while (!file.eof());
+        
+        */
+        file.seekg(0, ios::end);
+        cout<<file.tellg()<<endl;
+
     }
     file.close();
     return 0;
