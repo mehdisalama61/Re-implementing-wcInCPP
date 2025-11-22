750582223420ab1e1729ca6545ffdda1da967ead
elmehdisalama61 <mehdisalama61@gmail.com>
2025-11-22 18:17:43 +0100

    kk

--- wc Implementation In C++/wc.cpp
+++ wc Implementation In C++/wc.cpp
@@ -1,3 +1,15 @@
-int main(int argc,char argv**){
-    
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main(){
+    fstream file;
+    file.open("text.txt", ios::in | ios::out | ios :: binary);
+    if (file.is_open())
+    {
+        cout<<"hello"<<endl;
+    }
+    file.close();
+    return 0;
 }
