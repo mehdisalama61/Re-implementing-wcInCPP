754ddbb0a464db9e4ebd7323f7d891ea2e080c71
elmehdisalama61 <mehdisalama61@gmail.com>
2025-11-29 21:34:38 +0100

    step 5

--- wc Implementation In C++/wc.cpp
+++ wc Implementation In C++/wc.cpp
@@ -3,13 +3,19 @@
 
 using namespace std;
 
-int nbytes(fstream &file){
+void reset(fstream &file){
+    file.clear();
+    file.seekg(0, ios::beg);
+}
 
+int nbytes(fstream &file){
+    reset(file);
     file.seekg(0, ios::end);
     return file.tellg();
 }
 
 int nlines(fstream &file){
+    reset(file);
     string line;
     int lines = 0;
     
@@ -21,6 +27,7 @@ int nlines(fstream &file){
 }
 
 int nwords(fstream &file){
+    reset(file);
     string line;
     int words = 0;
     while(getline(file,line)){
@@ -41,6 +48,7 @@ int nwords(fstream &file){
 }
 
 int nchars(fstream &file){
+    reset(file);
     string line;
     int chars = 0;
     while(getline(file,line)){
@@ -58,35 +66,52 @@ int main(int argc, char *argv[]){
         cout<<"Error."<<endl;
         return 1;
     }
-    string filename = argv[2];
 
-    fstream file(filename, ios::in | ios::out | ios::binary);
-    
-    string flag = argv[1];
-    
-    if(!file.is_open()){
-        cout<<"Error. Couldn't open the file specified."<<endl;
-        return 1;
+    else if (argc == 2)
+    {
+        string filename = argv[1];
+        fstream file(filename, ios::in | ios::out | ios::binary);
+        if(!file.is_open()){
+            cout<<"Error. Couldn't open the file specified."<<endl;
+            return 1;
+        }
+        cout<<nbytes(file)<<endl;
+        cout<<nlines(file)<<endl;
+        cout<<nwords(file)<<endl;
+        cout<<nchars(file)<<endl;
+        file.close();
+        return 0;
     }
+    if (argc == 3){
+        string filename = argv[2];
 
-    if (flag == "-c") {
-        cout << nbytes(file) << endl;
-    }
-    else if (flag == "-l") {
-        cout << nlines(file) << endl;
-    }
-    else if (flag == "-w") {
-        cout << nwords(file) << endl;
-    }
-    else if (flag == "-m") {
-        cout << nchars(file) << endl;
-    }
+        fstream file(filename, ios::in | ios::out | ios::binary);
+        
+        string flag = argv[1];
+        
+        if(!file.is_open()){
+            cout<<"Error. Couldn't open the file specified."<<endl;
+            return 1;
+        }
 
-    else {
-        cout << "Unknown flag: " << flag << endl;
-        return 1;
-    }
+        if (flag == "-c") {
+            cout << nbytes(file) << endl;
+        }
+        else if (flag == "-l") {
+            cout << nlines(file) << endl;
+        }
+        else if (flag == "-w") {
+            cout << nwords(file) << endl;
+        }
+        else if (flag == "-m") {
+            cout << nchars(file) << endl;
+        }
 
-    file.close();
-    return 0;
+        else {
+            cout << "Unknown flag: " << flag << endl;
+            return 1;
+        }
+        file.close();
+        return 0;
+    }
 }
