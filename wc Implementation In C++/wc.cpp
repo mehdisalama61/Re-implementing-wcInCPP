c2f7e16b47be700af29a7695775e1de41ac06035
elmehdisalama61 <mehdisalama61@gmail.com>
2025-11-29 19:37:42 +0100

    steps2 and 3

--- wc Implementation In C++/wc.cpp
+++ wc Implementation In C++/wc.cpp
@@ -3,27 +3,42 @@
 
 using namespace std;
 
-int main(){
-    fstream file;
-    file.open("text.txt", ios::in | ios::out | ios :: binary);
-    if (file.is_open())
-    {
-        /*
-        
-        string buffer;
-        do
-        {
-            
-            file >> buffer;
-            cout << buffer<< endl;
-            cout<<file.tellg()<<endl;
-        } while (!file.eof());
-        
-        */
-        file.seekg(0, ios::end);
-        cout<<file.tellg()<<endl;
+int nbytes(fstream &file){
 
+    file.seekg(0, ios::end);
+    return file.tellg();
+}
+
+int nlines(fstream &file){
+    string line;
+    int lines = 0;
+    
+    while (getline(file,line)){
+        lines++;
     }
-    file.close();
+    return lines;
+}
+
+int nwords(fstream &file){
+    string line;
+    int words = 0;
+    while(getline(file,line)){
+        int i = 1;
+        while(line[i]){
+            if ((isspace(line[i])) && !(isspace(i-1)))
+            {
+                words++;
+            }
+            i++;
+        }
+    }
+    return words;
+}
+
+
+
+int main(int argc, char *argv[]){
+    
+    
     return 0;
 }
